8fc3d44bdbf00fdd1a76ceaf45134d0f5077a2ab
alexander-mann <amann9@myseneca.ca>
2018-06-22 10:19:43 -0400

    init

---
+++ src/ProjectsPanel.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Panel from './Panel';
+import axios from 'axios';
+import moment from 'moment';
+import {Link} from 'react-router-dom';
+
+class ProjectsPanel extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        }
+    }
+    componentDidMount() {
+        axios.get("https://web422-app.herokuapp.com/projects").then((res) => {
+            this.setState({
+                projects: res.data
+            });
+        }).catch((err) => {
+            console.log("error")
+        });
+    }
+    render() {
+        return (
+            <Panel title="Projects">
+                <div className="table-responsive overview-table">
+                    <table className="table table-striped table-bordered">
+                        <tbody>
+                            {
+                                this.state.projects.map(function (project) {
+                                    let today = moment();
+                                    let date = moment(today).diff(project.ProjectStartDate, 'days');
+                                    project.ProjectStartDate = date;
+                                    return (
+                                        <tr key={project._id}>
+                                            <td>{project.ProjectName}</td>
+                                            <td>Active: {project.ProjectStartDate} Days</td>
+                                        </tr>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <Link to="/projects" className="btn btn-default form-control">View All Project Data</Link>
+            </Panel>
+        );
+    }
+}
+
+export default ProjectsPanel;
